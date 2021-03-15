6d49ff37a124d72be1889365fa5ab3b799fbd4d5
Arthur Bauman <bitbucket@bauman.nu>
2021-03-15 15:49:59 +0100

    Fixed a bug in docstring comment.

--- message.py
+++ message.py
@@ -9,15 +9,17 @@ from tzlocal import get_localzone
 class Message:
     @staticmethod
     def create(message_text='Unknown message '):
-    """Create message with date, time zone and line number.
+        '''
+            Create message with date, time zone and line number.
 
-    Args:
-        message_text (str): Raw message.
+             Args:
+                message_text (str): Raw message.
 
-    Returns:
-        str: Message with date, time, time zone, file (caller)
-             and line number (caller).
-    """
+            Returns:
+                str: Message with date, time, time zone, file (caller)
+                and line number (caller).
+        '''
+
         caller = getframeinfo(stack()[1][0])
         return("%s (%s) %s File: %s Line: %s\n" % (
             datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
